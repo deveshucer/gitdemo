348f6942d7282002440cac68cca5e18d469a6816
Devesh <deveshucer@gmail.com>
2025-02-21 10:25:02 +0530

    adding syslog

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello word!");
+
+        System.out.println("hello git webhook trigger!");
+
     }
 }
