2c98d56e303972113387e27c6086fdff02dc1b61
Devesh <deveshucer@gmail.com>
2025-02-21 16:11:21 +0530

    simple update

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello word!");
-
+        System.out.println("hello word demo!");
         System.out.println("hello git webhook trigger!");
 
     }
